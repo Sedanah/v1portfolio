e8b0b2a8fa61096f2b6e0ce94474e79946ad6bdd
Sedanah Mashhour <mashhour.h@northeastern.edu>
2024-01-27 16:42:52 -0500

    updated layout

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
       mouseControls: true,
       touchControls: true,
       gyroControls: false,
-      minHeight: 200.0,
+      minHeight: 400.0,
       minWidth: 200.0,
       maxDistance: 26.00,
-      spacing: 13.00,
+      spacing: 14.00,
       scale: 1.0,
       scaleMobile: 1.0,
       color: 0x283bca,
@@ -35,29 +35,31 @@ function App() {
       <div className="flex flex-col bg-[#172554] h-screen" ref={myRef2}>
         <Navigation />
         <div className="h-full w-full flex flex-col text-white items-center justify-center px-20 ">
-          <h2 className="text-2xl p-5 text-[#b4b4b4] ">Hello, I am</h2>
-          <h1 className="text-6xl font-bold text-[#E6E6FA] px-5 py-2">
+         
+          <h1 className="text-6xl font-bold text-[#64f5ffbd] px-5 py-2">
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
-                'Sedanah',
-                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Hello',
+                1000,
+                'Welcome to my website',
+                1000,
+                'I am Sedanah',
+                1000, // wait 1s before replacing first "" with second ""
                 'a Developer',
                 1000,
                 'a Designer',
                 1000,
-                '',
+                'Enjoy',
                 1000,
               ]}
               wrapper="span"
-              speed={30}
+              speed={20}
               style={{ fontSize: '1.6em', display: 'inline-block' }}
               repeat={Infinity}
             />
           </h1>
-          <h2 className="text-3xl font-bold text-gray-400 px-5 py-2 mt-5">
-            Crafting digital experiences.
-          </h2>
+         
         </div>
       </div>
 
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,11 +9,9 @@ export const Navigation = () => {
           <div className="flex items-center text-white gap-5">
             <Bomb className="h-8 w-8 text-white" />
           </div>
-          <div className="flex flex-wrap items-center text-white gap-10 ">
-              <a href={"/about"} className="transition ease-in-out delay-150hover:-translate-y-1 hover:scale-110 duration-300 hover:text-[#64ffda]"> About</a>
-              <a href={"/experience"} className="transition ease-in-out delay-150hover:-translate-y-1 hover:scale-110 duration-300 hover:text-[#64ffda]">Experience</a>
-              <a href={"/Contact"}  className="transition ease-in-out delay-150hover:-translate-y-1 hover:scale-110 duration-300 hover:text-[#64ffda]">Contact</a>
-              <button className="border-2 border-[#64ffda] rounded-md text-[#64ffda] px-3 py-2
+          <div className="flex flex-wrap items-center text-[#64f5ff] gap-10 ">
+             
+              <button className="border-2 border-[#64f5ff] rounded-md text-[#64f5ff] px-3 py-2
               transition hover:-translate-x-2 hover:-translate-y-2 ease-in-out animate-pulse">
                   Resume
               </button>
