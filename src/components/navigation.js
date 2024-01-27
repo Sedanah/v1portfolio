fdabb69a681dc9e29671954dfdf18edd702bad2a
Sedanah Mashhour <mashhour.h@northeastern.edu>
2024-01-27 16:51:47 -0500

    updated layout

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -13,7 +13,7 @@ export const Navigation = () => {
              
               <button className="border-2 border-[#64f5ff] rounded-md text-[#64f5ff] px-3 py-2
               transition hover:-translate-x-2 hover:-translate-y-2 ease-in-out animate-pulse">
-                  Resume
+                  Home
               </button>
           </div>
         </div>
